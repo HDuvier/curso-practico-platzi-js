18671bfa5764c558c81c08776ba24e567aab7b67
Hector Castro <douvcka06@gmail.com>
2022-06-04 23:51:31 -0500

    se hacen los formularios del circulo y triangulo

--- figuras.js
+++ figuras.js
@@ -19,7 +19,7 @@ function perimetroTriangulo(ladoTriangulo1,ladoTriangulo2,baseTriangulo) {
 };
 
 function areaTriangulo (baseTriangulo, alturaTriangulo){    
-    return(baseTriangulo * alturaTriangulo)/2;
+    return((baseTriangulo*alturaTriangulo)/2);
 };
 
 console.groupEnd();
@@ -43,3 +43,59 @@ function areaCirculo(radio){
 }
 
 console.groupEnd();
+//enlazamos con html
+
+function calcPerimetroCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const Perimetro = perimetroCuadrado(value);
+    alert(Perimetro);
+};
+
+function calcAreaCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = Number(input.value);
+
+    const Area = areaCuadrado(value);
+    alert(Area);
+};
+
+function calcPerimetroTriangulo(){
+    const inputA = document.getElementById("inputLadoA");
+    const inputB = document.getElementById("inputLadoB");
+    const inputBa = document.getElementById("inputBase");
+    const valueA = Number (inputA.value);
+    const valueB = Number(inputB.value);
+    const valueBa = Number(inputBa.value);
+
+    let PerimetroTriangulo = perimetroTriangulo(valueA, valueB, valueBa);
+    alert(PerimetroTriangulo);
+};
+
+function calcAreaTriangulo() {
+    const inputBa = document.getElementById("inputBase");
+    const inputAlt = document.getElementById("inputAltura");
+    const valueBa = Number(inputBa.value);
+    const valueAlt = Number(inputAlt.value);
+    
+
+    let AreaTriangulo = areaTriangulo(valueBa, valueAlt);
+    alert(AreaTriangulo);
+};
+
+function calcPerimetroCirculo() {
+    const inputR = document.getElementById("inputRadio");
+    const valueR = inputR.value;
+
+    const PerimetroCir = perimetroCirculo(valueR);
+    alert(PerimetroCir);
+};
+
+function calcAreaCirculo() {
+    const inputR = document.getElementById("inputRadio");
+    const valueR = Number(inputR.value);
+
+    const AreaCir = areaCirculo(valueR);
+    alert(AreaCir);
+};
